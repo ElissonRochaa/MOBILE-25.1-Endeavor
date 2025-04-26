5917b0cd86a5cb19c5a30df5047eca5d298899cd
m-rero <bws.rrodrigues@gmail.com>
2025-04-26 03:18:06 -0300

    fix: main e google sign-in buttom

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:endeavor/screens/loginScreen.dart';
+import 'package:endeavor/screens/materiasScreen.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+
+import 'config/themeApp.dart';
+
+void main() async {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Endeavor',
+      theme: ThemeApp.theme,
+      home: //const DismissKeyboard(child: MateriasScreen()),
+      const DismissKeyboard(child: LoginScreen())
+    );
+  }
+}
+
+class DismissKeyboard extends StatelessWidget {
+  final Widget child;
+
+  const DismissKeyboard({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      behavior: HitTestBehavior.translucent,
+      child: child,
+    );
+  }
+}
+
+
