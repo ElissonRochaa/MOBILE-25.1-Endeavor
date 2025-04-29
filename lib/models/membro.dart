878a839a13be99350fb9e677636f62d3c6503ab0
ErickMartins153 <121458723+ErickMartins153@users.noreply.github.com>
2025-04-29 13:56:34 -0300

    feat: adicionar lógica de cronometrar tempo e service de membro

--- lib/models/membro.dart
+++ lib/models/membro.dart
@@ -1,21 +1,29 @@
 class Membro {
+  String id;
   String nome;
   String materia;
   bool isAtivo;
-  DateTime inicioEstudo;
+  DateTime? inicioEstudo;
+  Duration tempoTotalEstudado;
 
   Membro({
+    required this.id,
     required this.nome,
     required this.isAtivo,
     required this.materia,
-    required this.inicioEstudo,
-  });
+    this.inicioEstudo,
+    Duration? tempoTotalEstudado,
+  }) : tempoTotalEstudado = tempoTotalEstudado ?? Duration.zero;
 
   String tempoEstudoFormatado() {
-    final ducacao = DateTime.now().difference(inicioEstudo);
-    final horas = ducacao.inHours.toString().padLeft(2, '0');
-    final minutos = (ducacao.inMinutes % 60).toString().padLeft(2, '0');
-    final segundos = (ducacao.inSeconds % 60).toString().padLeft(2, '0');
+    final total =
+        isAtivo
+            ? tempoTotalEstudado + DateTime.now().difference(inicioEstudo!)
+            : tempoTotalEstudado;
+
+    final horas = total.inHours.toString().padLeft(2, '0');
+    final minutos = (total.inMinutes % 60).toString().padLeft(2, '0');
+    final segundos = (total.inSeconds % 60).toString().padLeft(2, '0');
     return "$horas:$minutos:$segundos";
   }
 }
