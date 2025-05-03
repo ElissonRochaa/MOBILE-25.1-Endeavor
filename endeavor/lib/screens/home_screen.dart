50260f550d4a45c6de032b6de860e436e8393338
RonyRangelSS <rony.rangel@upe.br>
2025-05-03 14:21:08 -0300

    Feat: Adicionando áreas dde estudo a home screen

--- endeavor/lib/screens/home_screen.dart
+++ endeavor/lib/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:endeavor/models/materia.dart';
 import 'package:endeavor/services/grupo_service.dart';
 import 'package:endeavor/widgets/geral/endeavor_bottom_bar.dart';
 import 'package:endeavor/widgets/geral/endeavor_top_bar.dart';
+import 'package:endeavor/widgets/home/areas_estudo.dart';
 import 'package:endeavor/widgets/home/card_grupo.dart';
 import 'package:endeavor/widgets/home/label.dart';
 import 'package:endeavor/widgets/materias/materia_item.dart';
@@ -40,10 +41,20 @@ class HomeScreen extends StatelessWidget {
           Container(
             margin: EdgeInsets.symmetric(vertical: 20, horizontal: 28),
             child: SearchBar(
+              
               leading: Icon(Icons.search, size: 24, color: Colors.grey[600]),
               padding: WidgetStatePropertyAll(EdgeInsets.all(8)),
               backgroundColor: WidgetStateProperty.all(
                 Theme.of(context).colorScheme.surface,
+              ),
+              shape: WidgetStatePropertyAll<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(40),
+                  side: BorderSide(
+                    color: Colors.grey,
+                    width: 1.0,
+                  ),
+                ),
               ),
               trailing: [
                 Icon(Icons.reorder, size: 24, color: Colors.grey[600]),
@@ -52,7 +63,7 @@ class HomeScreen extends StatelessWidget {
           ),
           Label(title: "Minhas matérias"),
           SizedBox(
-            height: 180,
+            height: 120,
             child: ListView.builder(
               itemCount: materias.length,
               scrollDirection: Axis.horizontal,
@@ -60,6 +71,7 @@ class HomeScreen extends StatelessWidget {
                   (ctx, index) => MateriaItem(materia: materias[index]),
             ),
           ),
+          SizedBox(height: 40),
           Label(title: "Meus grupos"),
           Container(
             padding: EdgeInsets.only(left: 20),
@@ -74,15 +86,26 @@ class HomeScreen extends StatelessWidget {
                 } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
                   return Center(child: Text('Nenhum grupo encontrado'));
                 }
-            
+
                 final grupos = snapshot.data!;
-            
+
                 return ListView.builder(
                   itemCount: grupos.length,
                   scrollDirection: Axis.horizontal,
                   itemBuilder: (ctx, index) => CardGrupo(grupo: grupos[index]),
                 );
               },
+            ),
+          ),
+          SizedBox(height: 40),
+          Label(title: "Áreas de estudo"),
+          Container(
+            padding: EdgeInsets.only(left: 20),
+            height: 140,
+            child: ListView.builder(
+              itemCount: 5,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (ctx, index) => AreasEstudo(nome: "Mobile"),
             ),
           ),
         ],
