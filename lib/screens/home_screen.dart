d0e192af74c5ecb5bdabc5681d748234bc3f2746
ErickMartins153 <121458723+ErickMartins153@users.noreply.github.com>
2025-05-08 14:23:06 -0300

    fix: arrumar pequenos problemas visuais e de layout

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:endeavor/services/grupo_service.dart';
 import 'package:endeavor/services/materia_service.dart';
 import 'package:endeavor/widgets/geral/endeavor_bottom_bar.dart';
@@ -6,7 +7,6 @@ import 'package:endeavor/widgets/home/areas_estudo.dart';
 import 'package:endeavor/widgets/home/card_grupo.dart';
 import 'package:endeavor/widgets/home/label.dart';
 import 'package:endeavor/widgets/materias/materia_item.dart';
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -16,102 +16,101 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: EndeavorTopBar(title: "Endeavor"),
       bottomNavigationBar: EndeavorBottomBar(),
-      body: Column(
-        children: [
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 20, horizontal: 28),
-            child: SearchBar(
-              
-              leading: Icon(Icons.search, size: 24, color: Colors.grey[600]),
-              padding: WidgetStatePropertyAll(EdgeInsets.all(8)),
-              backgroundColor: WidgetStateProperty.all(
-                Theme.of(context).colorScheme.surface,
-              ),
-              shape: WidgetStatePropertyAll<RoundedRectangleBorder>(
-                RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(40),
-                  side: BorderSide(
-                    color: Colors.grey,
-                    width: 1.0,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 20, horizontal: 28),
+              child: SearchBar(
+                leading: Icon(Icons.search, size: 24, color: Colors.grey[600]),
+                padding: WidgetStatePropertyAll(EdgeInsets.all(8)),
+                backgroundColor: WidgetStateProperty.all(
+                  Theme.of(context).colorScheme.surface,
+                ),
+                shape: WidgetStatePropertyAll<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(40),
+                    side: BorderSide(color: Colors.grey, width: 1.0),
                   ),
                 ),
+                trailing: [
+                  Icon(Icons.reorder, size: 24, color: Colors.grey[600]),
+                ],
               ),
-              trailing: [
-                Icon(Icons.reorder, size: 24, color: Colors.grey[600]),
-              ],
             ),
-          ),
-          Label(title: "Minhas matérias"),
-          SizedBox(
-            height: 200,
-            child: FutureBuilder(
-              future: getMaterias(),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return Center(child: CircularProgressIndicator());
-                } else if (snapshot.hasError) {
-                  return Center(child: Text('Erro: ${snapshot.error}'));
-                } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                  return Center(child: Text('Nenhuma matéria encontrado'));
-                }
+            Label(title: "Minhas matérias"),
+            SizedBox(
+              height: 200,
+              child: FutureBuilder(
+                future: getMaterias(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(child: CircularProgressIndicator());
+                  } else if (snapshot.hasError) {
+                    return Center(child: Text('Erro: ${snapshot.error}'));
+                  } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                    return Center(child: Text('Nenhuma matéria encontrado'));
+                  }
 
-                final materias = snapshot.data!; 
+                  final materias = snapshot.data!;
 
-              return CarouselSlider(
-                options: CarouselOptions(height: 400.0),
-                items: materias.map((materia) {
-                  return Builder(
-                    builder: (BuildContext context) {
-                      return MateriaItem(materia: materia);
-                    },
+                  return CarouselSlider(
+                    options: CarouselOptions(height: 400.0),
+                    items:
+                        materias.map((materia) {
+                          return Builder(
+                            builder: (BuildContext context) {
+                              return MateriaItem(materia: materia);
+                            },
+                          );
+                        }).toList(),
                   );
-                }).toList(),
-              );
-              } 
-            )
-          ),
-          SizedBox(height: 40),
-          Label(title: "Meus grupos"),
-          Container(
-            padding: EdgeInsets.only(left: 20),
-            height: 70,
-            child: FutureBuilder(
-              future: getGrupos(),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return Center(child: CircularProgressIndicator());
-                } else if (snapshot.hasError) {
-                  return Center(child: Text('Erro: ${snapshot.error}'));
-                } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                  return Center(child: Text('Nenhum grupo encontrado'));
-                }
-
-                final grupos = snapshot.data!;
-
-                return ListView.builder(
-                  itemCount: grupos.length,
-                  scrollDirection: Axis.horizontal,
-                  itemBuilder: (ctx, index) => CardGrupo(grupo: grupos[index]),
-                );
-              },
+                },
+              ),
             ),
-          ),
-          SizedBox(height: 40),
-          Label(title: "Áreas de estudo"),
-          Container(
-            padding: EdgeInsets.only(left: 20),
-            height: 140,
-            child: ListView.builder(
-              itemCount: 5,
-              scrollDirection: Axis.horizontal,
-              itemBuilder: (ctx, index) => AreasEstudo(nome: "Mobile"),
+            SizedBox(height: 40),
+            Label(title: "Meus grupos"),
+            Container(
+              padding: EdgeInsets.only(left: 20),
+              height: 70,
+              child: FutureBuilder(
+                future: getGrupos(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(child: CircularProgressIndicator());
+                  } else if (snapshot.hasError) {
+                    return Center(child: Text('Erro: ${snapshot.error}'));
+                  } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                    return Center(child: Text('Nenhum grupo encontrado'));
+                  }
+
+                  final grupos = snapshot.data!;
+
+                  return ListView.builder(
+                    itemCount: grupos.length,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder:
+                        (ctx, index) => CardGrupo(grupo: grupos[index]),
+                  );
+                },
+              ),
             ),
-          ),
-        ],
+            SizedBox(height: 40),
+            Label(title: "Áreas de estudo"),
+            Container(
+              padding: EdgeInsets.only(left: 20),
+              height: 140,
+              child: ListView.builder(
+                itemCount: 5,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (ctx, index) => AreasEstudo(nome: "Mobile"),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
