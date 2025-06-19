3fd9fc90727a40e1a9a65619b795668cffc0af57
m-rero <bws.rrodrigues@gmail.com>
2025-06-18 22:16:35 -0300

    feat: estatisticas e tema escuro

--- lib/config/theme_app.dart
+++ lib/config/theme_app.dart
@@ -1,9 +1,8 @@
-
 import 'package:flutter/material.dart';
 
 class ThemeApp {
   static final theme = ThemeData(
-    colorScheme: const ColorScheme(
+    colorScheme: ColorScheme(
       brightness: Brightness.light,
       primary: Color(0xFF03045E),
       onPrimary: Colors.white,
@@ -14,16 +13,13 @@ class ThemeApp {
       tertiaryContainer: Color(0xFFCAF0F8),
       error: Colors.red,
       onError: Colors.white,
-      surface: Color(0xFF00B4D8),
-      onSurface: Colors.white,
+      surface: Colors.white,
+      onSurface: Colors.black,
     ),
-    scaffoldBackgroundColor: Color(0xFF00B4D8),
-    cardColor: Color(0xFF00B4D8),
-    canvasColor: Color(0xFF00B4D8),
   );
 
   static final darkTheme = ThemeData(
-    colorScheme: const ColorScheme(
+    colorScheme: ColorScheme.dark(
       brightness: Brightness.dark,
       primary: Color(0xFF0077B6),
       onPrimary: Colors.white,
@@ -33,12 +29,10 @@ class ThemeApp {
       onTertiary: Colors.white,
       tertiaryContainer: Color(0xFFCAF0F8),
       error: Color(0xFFEF476F),
-      onError: Colors.black,
-      surface: Color(0xFF757575),
+      onError: Colors.white,
+      surface: Color(0xFF2F2D2D),
       onSurface: Colors.white,
     ),
-    scaffoldBackgroundColor: Color(0xFF757575),
-    cardColor: Color(0xFF757575),
-    canvasColor: Color(0xFF757575),
   );
+
 }
